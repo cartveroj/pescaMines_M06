4400524a33566d55b443e367fd9bc4e235d8a770
cartveroj <cartagenaveronica.19@gmail.com>
2023-11-29 23:39:53 +0100

    avance buscaminas

--- js/pescamines.js
+++ js/pescamines.js
@@ -1,6 +1,7 @@
 
 let Gfilas = 0;
 let Gcolumnas = 0;
+let GcasillasRestantes = 0;
 
 function getFilas(){
     
@@ -15,7 +16,7 @@ function iniciarPartida(){
     let valorMinimo = 10;
     let valorMaximo = 30;
 
-    let numFilCol = parseInt(prompt("Introduce un numero (min 10 y max 30)"));
+    let numFilCol = parseInt(prompt("Introduce un numero (min 10 y max 30)", "10"));
    
     if(numFilCol<10){
         numFilCol = valorMinimo;
@@ -28,7 +29,9 @@ function iniciarPartida(){
     Gcolumnas = numFilCol;
     
     crearTabla(numFilCol);
-    setMines();
+    let minas = setMines();
+    GcasillasRestantes = (Gfilas*Gcolumnas)-minas;
+    console.log("casillas restantes"+GcasillasRestantes);
     calculaAdjacents();
 }
 
@@ -40,8 +43,8 @@ function crearTabla(numFilCol){
    for (var i = 0; i < numFilCol; i++) {
         tabla += `<tr width="20px" height="20px">`;
         for(let j = 0; j < numFilCol; j++){
-            tabla += `<td width="20px" height="20px" data-num-mines="" id="${"td."+i+"."+j}" data-mina = "false" >
-            <img src="img_pescamines/fons20px.jpg"  id="${i+"."+j}" onclick="obreCasella(${i+"."+j})" >
+            tabla += `<td width="20px" height="20px" data-num-mines="" id="${"td."+i+"."+j}" data-mina = 'false' descubierta="false">
+            <img oncontextmenu = "ponerBandera(${i}, ${j})" src="img_pescamines/fons20px.jpg"  id="${i+"."+j}" onclick="obreCasella(${i}, ${j})" >
             </td>`
         }
         tabla +=`</tr>`
@@ -52,41 +55,115 @@ function crearTabla(numFilCol){
     document.getElementById('taulell').innerHTML = tabla;
 }
 
-function obreCasella(posicion){
-    let arrayPos = posicion.toString();
-    let x = arrayPos.charAt(0);
-    let y = arrayPos.charAt(2);
-    console.log(x+"+"+y);
-    console.log(esMina(x,y));
+function obreCasella(x,y){
+    let esUnaMina = esMina(x,y);
+    let celda = document.getElementById("td."+ x+"."+y);
+    let imagen = document.getElementById(x+'.'+y);
+    let perdioEljuego = false;
+    let tabla = document.getElementById('miTabla');
+    let numeroMinasAdjacents = celda.getAttribute('data-num-mines');
+    if(esUnaMina == "true"){
+        imagen.setAttribute('src', 'img_pescamines/mina20px.jpg');
+        
+        obreTodasLasMinas();
+        perdioEljuego = true;
+    }else{
+        if(numeroMinasAdjacents !== "" && celda.getAttribute('descubierta') == "false" ){
+            imagen.setAttribute('src', ' ');
+            tabla.rows[x].cells[y].innerHTML = `<p width="20px" height="20px">${numeroMinasAdjacents}</p>`
+            celda.setAttribute('descubierta','true');
+            GcasillasRestantes--;
+        }else if(celda.getAttribute('descubierta') == "false"){
+            imagen.setAttribute('src', ' ');
+            celda.setAttribute('descubierta','true');
+            GcasillasRestantes--;
+            for (let i = x - 1; i <= x + 1; i++) {
+                for (let j = y - 1; j <= y + 1; j++) {
+                    if (i >= 0 && i < tabla.rows.length && j >= 0 && j < tabla.rows.length
+                        && !(i === x && j === y)) {  
+                            obreCasella(i, j);
+                    }   
+                }
+            }
+        }
+        
+    }
+    console.log("me quedan: "+GcasillasRestantes);
+    if(perdioEljuego){
+        alert("Perdió el juego");
+        desHabilitarCasillas();
+    }else{
+        if(GcasillasRestantes == 0){
+            alert('Ganaste..¡¡');
+            desHabilitarCasillas();
+        }
+    }
+    
+    
 }
 
 
+
+function desHabilitarCasillas() {
+    let imagenes = document.getElementsByTagName("img");
+    // Hacer una copia de la lista de imágenes para evitar problemas de tiempo real
+    let imagenesArray = Array.from(imagenes);
+    for (let i = 0; i < imagenesArray.length; i++) {
+        imagenesArray[i].onclick = null;
+    }
+}
+function ponerBandera(x,y){
+    let imagen = document.getElementById(x+'.'+y);
+    imagen.setAttribute('src', 'img_pescamines/badera20px.jpg');
+}
+function obreTodasLasMinas(){
+    let tabla = document.getElementById('miTabla');
+
+    for(let i=0; i < tabla.rows.length ; i++){
+        for(let j=0; j < tabla.rows[i].cells.length ; j++){
+            let celda = document.getElementById("td."+ i+"."+j);
+            let tieneMina = celda.getAttribute('data-mina');
+            if(tieneMina == "true"){
+                let imagen = document.getElementById(i+'.'+j);
+                imagen.setAttribute('src', 'img_pescamines/mina20px.jpg');
+            }
+        }
+    }
+}
 
 function setMines(){
     let filas = getFilas();
     let columnas = getColumnas();
     let porcentaje = 0.17;
-    let cantidadDeMinas = (filas * columnas) * porcentaje;
+    let cantidadDeMinas = Math.floor((filas * columnas) * porcentaje);
     let contador=0;
 
     let tabla = document.getElementById('miTabla');
+    let arrayCeldas = [];
 
-    for(let i=0; i<= tabla.rows.length-1 ; i++){
-        for(let j=0; j <= tabla.rows[i].cells.length-1 ; j++){
+    for(let i=0; i < tabla.rows.length ; i++){
+        for(let j=0; j < tabla.rows[i].cells.length ; j++){
             let celda = document.getElementById("td."+ i+"."+j);
-            if (Math.random() < 0.17) {
-                celda.setAttribute('data-mina', 'true');
-                contador++;
-            } else {
-                celda.setAttribute('data-mina', 'false');
-            }
-            //let atributo = celda.getAttribute("data-mina");
-            
+            arrayCeldas.push(celda);
+
         }
     }
+     // Asignamos exactamente la cantidad deseada de minas
+     while (contador < cantidadDeMinas) {
+        let indiceAleatorio = Math.floor(Math.random() * arrayCeldas.length);
+        let celda = arrayCeldas[indiceAleatorio];
 
-    console.log(contador);
+        if (celda.getAttribute('data-mina') === 'false') {
+            celda.setAttribute('data-mina', 'true');
+            contador++;
+        }
 
+        // Eliminamos la celda del array para no seleccionarla de nuevo
+        arrayCeldas.splice(indiceAleatorio, 1);
+    }
+    console.log("Numero de minas:"+ cantidadDeMinas +"es igual: " + contador );
+
+    return cantidadDeMinas;
     
 }
 
@@ -95,19 +172,33 @@ function calculaAdjacents(){
     let tabla = document.getElementById('miTabla');
     for(let i = 0; i< tabla.rows.length; i++){
         for(let j=0; j< tabla.rows[i].cells.length; j++){
-           // console.log(tabla.rows[i].cells[j]);
+        // console.log("celda:");
+        // console.log(tabla.rows[i].cells[j]);
+           contadorMinas=0;
             let esUnaMina = esMina(i,j);
-            console.log(typeof(esUnaMina));
            if( esUnaMina == "false" ){
                 for (let x = i - 1; x <= i + 1; x++) {
                     for (let y = j - 1; y <= j + 1; y++) {
                         // Asegúrate de que las coordenadas no estén fuera de los límites de la tabla
-                        if (x >= 0 && x < tabla.rows.length && y >= 0 && y < tabla.rows[i].cells.length) {
+                        if (x >= 0 && x < tabla.rows.length && y >= 0 && y < tabla.rows[i].cells.length
+                            && !(x ==i && y==j)) {
                             // Accede a la celda vecina
+                            
                             let celdaVecina = tabla.rows[x].cells[y];
-                            console.log(celdaVecina);
+                            // console.log("celda vecina");
+                            // console.log(celdaVecina);
                             // Realiza las operaciones que necesites con la celda vecina
                             let tieneMina = celdaVecina.getAttribute('data-mina');
+                            //console.log("celda vecina tiene mina"+tieneMina);
+                            if(tieneMina == "true"){
+                                contadorMinas++
+                                setMinesAdjacents(i,j,contadorMinas);
+                                let celda = document.getElementById("td."+ i+"."+j);
+                                let numMinas = celda.getAttribute('data-num-mines');
+                                //console.log("celda con numero minas"+numMinas);;
+
+                            }
+                            
                         }
                     }
                 }
@@ -116,10 +207,14 @@ function calculaAdjacents(){
         }
     }
 
-    console.log(contadorMinas);
 }
 function esMina(x,y){
+    //console.log("revisando celda"+x +"y"+y);
     let celda = document.getElementById("td."+ x+"."+y);
     return celda.getAttribute('data-mina');
 }
-function setMinesAdjacents(){}
+function setMinesAdjacents(i,j,minasAdjacents){
+     let celda = document.getElementById("td."+ i+"."+j);
+     celda.setAttribute('data-num-mines', minasAdjacents);
+}
+
